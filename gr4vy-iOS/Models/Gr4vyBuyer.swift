3926fa32c616d2c5fb696b0ee7e9f31a19d6298d
Luca Allievi <luca@gr4vy.com>
2024-08-24 12:06:04 +0200

    Add missing properties and address / taxId structs

--- gr4vy-iOS/Models/Gr4vyBuyer.swift
+++ gr4vy-iOS/Models/Gr4vyBuyer.swift
@@ -8,10 +8,15 @@ import Foundation
 public struct Gr4vyBuyer: Codable {
     let displayName: String?
     let externalIdentifier: String?
-    let billingDetails: Gr4vyBillingDetails?
-    let shippingDetails: Gr4vyBillingDetails?
+    let billingDetails: Gr4vyBillingShippingDetails?
+    let shippingDetails: Gr4vyBillingShippingDetails?
     
-    public init(displayName: String? = nil, externalIdentifier: String? = nil, billingDetails: Gr4vyBillingDetails? = nil, shippingDetails: Gr4vyBillingDetails? = nil) {
+    public init(
+        displayName: String? = nil,
+        externalIdentifier: String? = nil,
+        billingDetails: Gr4vyBillingShippingDetails? = nil,
+        shippingDetails: Gr4vyBillingShippingDetails? = nil
+    ) {
         self.displayName = displayName
         self.externalIdentifier = externalIdentifier
         self.billingDetails = billingDetails
@@ -19,10 +24,74 @@ public struct Gr4vyBuyer: Codable {
     }
 }
 
-public struct Gr4vyBillingDetails: Codable {
+public struct Gr4vyBillingShippingDetails: Codable {
     let firstName: String?
-    
-    public init(firstName: String? = nil) {
+    let lastName: String?
+    let emailAddress: String?
+    let phoneNumber: String?
+    let address: Gr4vyAddress?
+    let taxId: Gr4vyTaxId?
+
+    public init(
+        firstName: String? = nil,
+        lastName: String? = nil,
+        emailAddress: String? = nil,
+        phoneNumber: String? = nil,
+        address: Gr4vyAddress? = nil,
+        taxId: Gr4vyTaxId? = nil
+    ) {
         self.firstName = firstName
+        self.lastName = lastName
+        self.emailAddress = emailAddress
+        self.phoneNumber = phoneNumber
+        self.address = address
+        self.taxId = taxId
+    }
+}
+
+public struct Gr4vyAddress: Codable {
+    let houseNumberOrName: String?
+    let line1: String?
+    let line2: String?
+    let organization: String?
+    let city: String?
+    let postalCode: String?
+    let country: String?
+    let state: String?
+    let stateCode: String?
+
+    public init(
+        houseNumberOrName: String? = nil,
+        line1: String? = nil,
+        line2: String? = nil,
+        organization: String? = nil,
+        city: String? = nil,
+        postalCode: String? = nil,
+        country: String? = nil,
+        state: String? = nil,
+        stateCode: String? = nil
+    ) {
+        self.houseNumberOrName = houseNumberOrName
+        self.line1 = line1
+        self.line2 = line2
+        self.organization = organization
+        self.city = city
+        self.postalCode = postalCode
+        self.country = country
+        self.state = state
+        self.stateCode = stateCode
+    }
+}
+
+public struct Gr4vyTaxId: Codable {
+    let value: String?
+    let kind: String?
+
+    public init(
+        value: String? = nil,
+        kind: String? = nil
+    ) {
+        self.value = value
+        self.kind = kind
     }
 }
